b9c49f967cd9e478bb4e6c9cc30b078ff379846f
Johan Brandhorst <johan.brandhorst@gmail.com>
2019-01-26 20:46:22 +0000

    Add AWS CA issuer
    Adds support for the AWS Certificate Manager Private
    Certificate Authority.

---
+++ issuers/aws/aws.go
@@ -0,0 +1,178 @@
+package aws
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"errors"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/acmpca"
+	iface "github.com/aws/aws-sdk-go-v2/service/acmpca/acmpcaiface"
+
+	"github.com/johanbrandhorst/certify"
+)
+
+// Issuer implements the Issuer interface with a
+// AWS Certificate Manager Private Certificate Authority backend.
+//
+// Client and CertificateAuthorityARN are required.
+type Issuer struct {
+	// Client is a pre-created ACMPCA client. It can be created
+	// via, for example:
+	//    conf, err := external.LoadDefaultAWSConfig()
+	//    if err != nil {
+	//        return nil, err
+	//    }
+	//    conf.Region = endpoints.EuWest2RegionID
+	//    conf.Credentials = aws.NewStaticCredentialsProvider("YOURKEY", "YOURKEYSECRET", "")
+	//    cli := acmpca.New(conf)
+	Client iface.ACMPCAAPI
+	// CertificateAuthorityARN specifies the ARN of a pre-created CA
+	// which will be used to issue the certificates.
+	CertificateAuthorityARN string
+
+	// TimeToLive configures the lifetime of certificates
+	// requested from the AWS CA, in number of days.
+	// If unset, defaults to 30 days.
+	TimeToLive int
+
+	caCert   *x509.Certificate
+	signAlgo acmpca.SigningAlgorithm
+}
+
+// Issue issues a certificate from the configured AWS CA backend.
+func (i Issuer) Issue(ctx context.Context, commonName string, conf *certify.CertConfig) (*tls.Certificate, error) {
+	if i.caCert == nil {
+		caReq := i.Client.GetCertificateAuthorityCertificateRequest(&acmpca.GetCertificateAuthorityCertificateInput{
+			CertificateAuthorityArn: aws.String(i.CertificateAuthorityARN),
+		})
+
+		caResp, err := caReq.Send()
+		if err != nil {
+			return nil, err
+		}
+
+		caBlock, _ := pem.Decode([]byte(*caResp.Certificate))
+		if caBlock == nil {
+			return nil, errors.New("could not parse AWS CA cert")
+		}
+
+		if caBlock.Type != "CERTIFICATE" {
+			return nil, errors.New("saw unexpected PEM Type while requesting AWS CA cert: " + caBlock.Type)
+		}
+
+		i.caCert, err = x509.ParseCertificate(caBlock.Bytes)
+		if err != nil {
+			return nil, err
+		}
+
+		switch i.caCert.SignatureAlgorithm {
+		case x509.SHA256WithRSA:
+			i.signAlgo = acmpca.SigningAlgorithmSha256withrsa
+		case x509.SHA384WithRSA:
+			i.signAlgo = acmpca.SigningAlgorithmSha384withrsa
+		case x509.SHA512WithRSA:
+			i.signAlgo = acmpca.SigningAlgorithmSha512withrsa
+		case x509.ECDSAWithSHA256:
+			i.signAlgo = acmpca.SigningAlgorithmSha256withecdsa
+		case x509.ECDSAWithSHA384:
+			i.signAlgo = acmpca.SigningAlgorithmSha384withecdsa
+		case x509.ECDSAWithSHA512:
+			i.signAlgo = acmpca.SigningAlgorithmSha512withecdsa
+		default:
+			return nil, fmt.Errorf("unsupported CA cert signing algorithm: %T", i.caCert.SignatureAlgorithm)
+		}
+	}
+
+	pk, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		return nil, err
+	}
+
+	keyBytes, err := x509.MarshalECPrivateKey(pk)
+	if err != nil {
+		return nil, err
+	}
+
+	keyPEM := pem.EncodeToMemory(&pem.Block{
+		Type:  "EC PRIVATE KEY",
+		Bytes: keyBytes,
+	})
+
+	template := &x509.CertificateRequest{
+		SignatureAlgorithm: x509.ECDSAWithSHA256,
+		Subject: pkix.Name{
+			CommonName: commonName,
+		},
+	}
+
+	if conf != nil {
+		template.DNSNames = conf.SubjectAlternativeNames
+		template.IPAddresses = conf.IPSubjectAlternativeNames
+	}
+
+	csr, err := x509.CreateCertificateRequest(rand.Reader, template, pk)
+	if err != nil {
+		return nil, err
+	}
+
+	csrPEM := pem.EncodeToMemory(&pem.Block{
+		Type:  "CERTIFICATE REQUEST",
+		Bytes: csr,
+	})
+
+	// Default to 30 days if unset.
+	ttl := int64(30)
+	if i.TimeToLive > 0 {
+		ttl = int64(i.TimeToLive)
+	}
+
+	csrReq := i.Client.IssueCertificateRequest(&acmpca.IssueCertificateInput{
+		CertificateAuthorityArn: aws.String(i.CertificateAuthorityARN),
+		Csr:                     csrPEM,
+		SigningAlgorithm:        i.signAlgo,
+		Validity: &acmpca.Validity{
+			Type:  acmpca.ValidityPeriodTypeDays,
+			Value: aws.Int64(ttl),
+		},
+	})
+
+	csrResp, err := csrReq.Send()
+	if err != nil {
+		return nil, err
+	}
+
+	getReq := &acmpca.GetCertificateInput{
+		CertificateArn:          csrResp.CertificateArn,
+		CertificateAuthorityArn: aws.String(i.CertificateAuthorityARN),
+	}
+	err = i.Client.WaitUntilCertificateIssuedWithContext(ctx, getReq)
+	if err != nil {
+		return nil, err
+	}
+
+	certReq := i.Client.GetCertificateRequest(getReq)
+
+	certResp, err := certReq.Send()
+	if err != nil {
+		return nil, err
+	}
+
+	caChainPEM := append([]byte(*certResp.Certificate), []byte(*certResp.CertificateChain)...)
+
+	tlsCert, err := tls.X509KeyPair(caChainPEM, keyPEM)
+	if err != nil {
+		return nil, err
+	}
+
+	// This can't error since it's called in tls.X509KeyPair above successfully
+	tlsCert.Leaf, _ = x509.ParseCertificate(tlsCert.Certificate[0])
+	return &tlsCert, nil
+}
---
+++ issuers/aws/aws_test.go
@@ -0,0 +1,229 @@
+package aws_test
+
+import (
+	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"math/big"
+	"net"
+	"time"
+
+	api "github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/acmpca"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gstruct"
+
+	"github.com/johanbrandhorst/certify"
+	"github.com/johanbrandhorst/certify/issuers/aws"
+	"github.com/johanbrandhorst/certify/issuers/aws/mocks"
+)
+
+//go:generate moq -out mocks/client.mock.go -pkg mocks ../../vendor/github.com/aws/aws-sdk-go-v2/service/acmpca/acmpcaiface ACMPCAAPI
+
+var _ = Describe("AWS Issuer", func() {
+	It("issues a certificate", func() {
+		caARN := "someARN"
+		certARN := "anotherARN"
+		caCert, caKey, err := generateCertAndKey()
+		Expect(err).To(Succeed())
+		client := &mocks.ACMPCAAPIMock{}
+		iss := &aws.Issuer{
+			CertificateAuthorityARN: caARN,
+			Client:                  client,
+			TimeToLive:              25,
+		}
+		var signedCert []byte
+		client.IssueCertificateRequestFunc = func(in1 *acmpca.IssueCertificateInput) acmpca.IssueCertificateRequest {
+			Expect(in1.CertificateAuthorityArn).To(PointTo(Equal(caARN)))
+			Expect(in1.Validity.Value).To(PointTo(BeEquivalentTo(iss.TimeToLive)))
+			b, _ := pem.Decode(in1.Csr)
+			csr, err := x509.ParseCertificateRequest(b.Bytes)
+			Expect(err).NotTo(HaveOccurred())
+			serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+			serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+			Expect(err).NotTo(HaveOccurred())
+			template := &x509.Certificate{
+				SerialNumber:       serialNumber,
+				Subject:            csr.Subject,
+				PublicKeyAlgorithm: csr.PublicKeyAlgorithm,
+				PublicKey:          csr.PublicKey,
+				SignatureAlgorithm: x509.SHA256WithRSA,
+				DNSNames:           csr.DNSNames,
+				IPAddresses:        csr.IPAddresses,
+				EmailAddresses:     csr.EmailAddresses,
+				URIs:               csr.URIs,
+				NotBefore:          time.Now(),
+				NotAfter:           time.Now().AddDate(0, 0, int(*in1.Validity.Value)),
+			}
+			crt, err := x509.CreateCertificate(rand.Reader, template, caCert.cert, csr.PublicKey, caKey.key)
+			Expect(err).NotTo(HaveOccurred())
+			signedCert = pem.EncodeToMemory(&pem.Block{
+				Type:  "CERTIFICATE",
+				Bytes: crt,
+			})
+			hl := api.HandlerList{}
+			hl.PushBackNamed(api.NamedHandler{
+				Name: "Send",
+				Fn: func(in *api.Request) {
+					in.Data = &acmpca.IssueCertificateOutput{
+						CertificateArn: api.String(certARN),
+					}
+				},
+			})
+			return acmpca.IssueCertificateRequest{
+				Request: &api.Request{
+					Handlers: api.Handlers{
+						Send: hl,
+					},
+				},
+				Input: in1,
+			}
+		}
+		client.GetCertificateRequestFunc = func(in1 *acmpca.GetCertificateInput) acmpca.GetCertificateRequest {
+			Expect(in1.CertificateArn).To(PointTo(Equal(certARN)))
+			Expect(in1.CertificateAuthorityArn).To(PointTo(Equal(caARN)))
+			hl := api.HandlerList{}
+			hl.PushBackNamed(api.NamedHandler{
+				Name: "Send",
+				Fn: func(in *api.Request) {
+					in.Data = &acmpca.GetCertificateOutput{
+						Certificate:      api.String(string(signedCert)),
+						CertificateChain: api.String(string(caCert.pem)),
+					}
+				},
+			})
+			return acmpca.GetCertificateRequest{
+				Request: &api.Request{
+					Handlers: api.Handlers{
+						Send: hl,
+					},
+				},
+				Input: in1,
+			}
+		}
+		client.GetCertificateAuthorityCertificateRequestFunc = func(in1 *acmpca.GetCertificateAuthorityCertificateInput) acmpca.GetCertificateAuthorityCertificateRequest {
+			Expect(in1.CertificateAuthorityArn).To(PointTo(Equal(caARN)))
+			hl := api.HandlerList{}
+			hl.PushBackNamed(api.NamedHandler{
+				Name: "Send",
+				Fn: func(in *api.Request) {
+					in.Data = &acmpca.GetCertificateAuthorityCertificateOutput{
+						Certificate:      api.String(string(caCert.pem)),
+						CertificateChain: api.String(string(caCert.pem)),
+					}
+				},
+			})
+			return acmpca.GetCertificateAuthorityCertificateRequest{
+				Request: &api.Request{
+					Handlers: api.Handlers{
+						Send: hl,
+					},
+				},
+				Input: in1,
+			}
+		}
+		client.WaitUntilCertificateIssuedWithContextFunc = func(in1 api.Context, in2 *acmpca.GetCertificateInput, in3 ...api.WaiterOption) error {
+			Expect(in2.CertificateAuthorityArn).To(PointTo(Equal(caARN)))
+			Expect(in2.CertificateArn).To(PointTo(Equal(certARN)))
+			hl := api.HandlerList{}
+			hl.PushBackNamed(api.NamedHandler{
+				Name: "Send",
+				Fn: func(in *api.Request) {
+					in.Data = &acmpca.GetCertificateAuthorityCertificateOutput{
+						Certificate:      api.String(string(caCert.pem)),
+						CertificateChain: api.String(string(caCert.pem)),
+					}
+				},
+			})
+			return nil
+		}
+
+		cn := "somename.com"
+		conf := &certify.CertConfig{
+			SubjectAlternativeNames:   []string{"extraname.com", "otherextraname.com"},
+			IPSubjectAlternativeNames: []net.IP{net.IPv4(1, 2, 3, 4), net.IPv6loopback},
+		}
+		tlsCert, err := iss.Issue(context.Background(), cn, conf)
+		Expect(err).NotTo(HaveOccurred())
+
+		Expect(tlsCert.Leaf).NotTo(BeNil(), "tlsCert.Leaf should be populated by Issue to track expiry")
+		Expect(tlsCert.Leaf.Subject.CommonName).To(Equal(cn))
+		Expect(tlsCert.Leaf.DNSNames).To(Equal(conf.SubjectAlternativeNames))
+		Expect(tlsCert.Leaf.IPAddresses).To(HaveLen(len(conf.IPSubjectAlternativeNames)))
+		for i, ip := range tlsCert.Leaf.IPAddresses {
+			Expect(ip.Equal(conf.IPSubjectAlternativeNames[i])).To(BeTrue())
+		}
+
+		// Check that chain is included
+		Expect(tlsCert.Certificate).To(HaveLen(2))
+		crt, err := x509.ParseCertificate(tlsCert.Certificate[1])
+		Expect(err).NotTo(HaveOccurred())
+		Expect(crt.Subject.SerialNumber).To(Equal(tlsCert.Leaf.Issuer.SerialNumber))
+
+		Expect(tlsCert.Leaf.NotBefore).To(BeTemporally("<", time.Now()))
+		Expect(tlsCert.Leaf.NotAfter).To(BeTemporally("~", time.Now().AddDate(0, 0, iss.TimeToLive), 5*time.Second))
+	})
+})
+
+type key struct {
+	pem []byte
+	key *rsa.PrivateKey
+}
+
+type cert struct {
+	pem  []byte
+	cert *x509.Certificate
+}
+
+func generateCertAndKey() (*cert, *key, error) {
+	priv, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return nil, nil, err
+	}
+	notBefore := time.Now()
+	notAfter := notBefore.Add(time.Hour)
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+	if err != nil {
+		return nil, nil, err
+	}
+	template := x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject: pkix.Name{
+			CommonName: "Certify Test Cert",
+		},
+		NotBefore:             notBefore,
+		NotAfter:              notAfter,
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+	}
+	derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, priv.Public(), priv)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	k := &key{
+		key: priv,
+		pem: pem.EncodeToMemory(&pem.Block{
+			Type:  "RSA PRIVATE KEY",
+			Bytes: x509.MarshalPKCS1PrivateKey(priv),
+		}),
+	}
+	crt, err := x509.ParseCertificate(derBytes)
+	if err != nil {
+		return nil, nil, err
+	}
+	c := &cert{
+		cert: crt,
+		pem: pem.EncodeToMemory(&pem.Block{
+			Type:  "CERTIFICATE",
+			Bytes: derBytes,
+		}),
+	}
+	return c, k, nil
+}
